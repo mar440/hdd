bae19241e97d7604d7979346986a06507f2523f0
mar440 <alex.markopoulos@gmail.com>
2020-03-31 16:53:21 +0200

    initialization with copied mpi communicator

--- include/data.hpp
+++ include/data.hpp
@@ -5,31 +5,52 @@
 #include <Eigen/Dense>
 #include "types.hpp"
 
+#include "domain.hpp"
+#include <mpi.h>
 
 
 
 class Data{
-  public:  
-    Data()  {}
+  public:
+    Data(MPI_Comm*);
     ~Data(){}
-    void assembly_elasticity(Mesh*);
 
-    void setDirichlet(Eigen::VectorXd& v,
-      std::vector<int> dirInd);
+    void SymbolicAssembling(std::vector<int>& glbIds);
+    void FinalizeSymbolicAssembling();
 
-    void setDirichlet(SpMat &mat, 
-      std::vector<int> dirInd);
-   
-    SpMat& getK(){return m_K;}
-    Eigen::VectorXd& getRHS(){return m_rhs;}
-    void printMatrix(SpMat& mat,std::string fname);
-    
-    Eigen::VectorXd svd0(SpMat& mat);
-    Eigen::VectorXd svd0(Eigen::MatrixXd& mat);
+    void NumericAssembling(std::vector<int>& glbIds,
+        std::vector<double>& valLocK,
+        std::vector<double>& valLocRHS);
+    void FinalizeNumericAssembling();
+
+    Domain& getDomain(){return  m_domain;}
+
+
 
   private:
-    SpMat m_K;
-    Eigen::VectorXd m_rhs;
+    MPI_Comm* m_pcomm;
+    std::vector<int> m_container;
+    Domain m_domain;
+
+    int m_cnt_setLocalMatrix;
+
+
+
+
+
+///////////////////////////////////////
+// build-in assembler for fast testing
+///////////////////////////////////////
+  public:
+//    void assembly_elasticity(Mesh*);
+//    void setDirichlet(Eigen::VectorXd& v,
+//        std::vector<int> dirInd);
+//    void setDirichlet(SpMat &mat,
+//        std::vector<int> dirInd);
+//    SpMat& getK(){return m_K;}
+//    Eigen::VectorXd& getRHS(){return m_rhs;}
+
+  private:
     void howMuchAssembled(int,int);
 
 };
