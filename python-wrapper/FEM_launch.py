21e15aa0d9234c4c24346ecbfd930c1018a9d696
mar440 <alex.markopoulos@gmail.com>
2020-05-10 00:54:31 +0200

    launcher for python mesh generator

---
+++ python-wrapper/FEM_launch.py
@@ -0,0 +1,100 @@
+from __future__ import division
+from __future__ import print_function
+
+
+import numpy as np
+from pythonHdd import PyHddApi
+from mpi4py import MPI
+
+import FEM_meshgenerator as m3d
+import FEM_assembler as assmbl
+
+
+nx = ny = nz = 2
+
+Nx = 2
+Ny = 1
+Nz = 1
+
+Lx = 1.0
+Ly = 1.0
+Lz = 1.0
+
+mpicomm = MPI.COMM_WORLD
+mpirank = mpicomm.rank
+
+
+print("start ... Python")
+hdd= PyHddApi(mpicomm)
+hdd.Message("Beginning ...")
+hdd.ParseJsonFile("hddConf.json")
+
+mesh = m3d.MeshGenerator({'Nx':Nx, 'Ny':Ny , 'Nz':Nz,
+            'nx':nx , 'ny':ny , 'nz' : nz,
+            'Lx':Lx , 'Ly':Ly , 'Lz' : Lz })
+
+
+dimension = 3
+Ex = 1.1
+mu = 0.3
+
+coordinates = mesh['coordinates']
+elements    = mesh['elements']
+edges       = mesh['edges']
+decomp      = mesh['decomp']
+nElements   = elements.shape[0]
+volF        = np.ones(3)
+
+DirichletFace = 0
+DirPts = \
+    np.unique(edges[edges[:,4]==DirichletFace,0:4].ravel())
+nDP = DirPts.shape[0]
+
+dirichletDofs = np.zeros(3 * nDP,dtype=np.int32)
+dirichletDofs[0::3] = 3 * DirPts + 0
+dirichletDofs[1::3] = 3 * DirPts + 1
+dirichletDofs[2::3] = 3 * DirPts + 2
+
+
+print(decomp)
+
+for i in range(nElements):
+    if (decomp[i] == mpirank):
+        ielem = elements[i,:]
+        nP =  ielem.shape[0]
+        neqLoc = nP * dimension
+        # ul = [ul1x,ul2x,ul3x,...., ul1y,ul2y,ul3y,.....]
+        indx = np.zeros(neqLoc,dtype=np.int32)
+        indx[0*nP:1*nP:] = ielem * 3 + 0
+        indx[1*nP:2*nP:] = ielem * 3 + 1
+        indx[2*nP:3*nP:] = ielem * 3 + 2
+        hdd.SymbolicAssembling(indx)
+###
+hdd.SetDirichletDOFs(dirichletDofs)
+hdd.FinalizeSymbolicAssembling()
+#
+for i in range(nElements):
+    if (decomp[i] == mpirank):
+        ielem = elements[i,:]
+        coordinatesLoc = coordinates[ielem,]
+        nP =  ielem.shape[0]
+        neqLoc = nP * dimension
+        indx = np.zeros(neqLoc,dtype=np.int32)
+        indx[0*nP:1*nP:] = ielem * 3 + 0
+        indx[1*nP:2*nP:] = ielem * 3 + 1
+        indx[2*nP:3*nP:] = ielem * 3 + 2
+
+        localElemData = assmbl.stima3e8({'coordinatesLoc':coordinatesLoc,
+            'Ex':Ex, 'mu':mu, 'volF':volF})
+        Ke = localElemData['Ke']
+        Fe = localElemData['fe']
+        hdd.NumericAssembling(indx,Ke,Fe)
+
+hdd.FinalizeNumericAssembling()
+
+
+solution = hdd.Solve()
+
+print(solution)
+
+if mpirank == 0: print("end ... Python")
