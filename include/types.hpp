9578f00472abb3a5ac14f2fbd6a0b42da2a22553
mar440 <alex.markopoulos@gmail.com>
2020-05-12 01:07:32 +0200

    dbg traces

--- include/types.hpp
+++ include/types.hpp
@@ -9,11 +9,14 @@ typedef Eigen::SparseMatrix<double, Eigen::RowMajor> SpMat;
 typedef Eigen::Triplet<double> T;
 enum PRECONDITIONER_TYPE {NONE, LUMPED, DIRICHLET};
 
-#define  DBGPRINT std::cout<< __FILE__ << ':'<<  __LINE__ << std::endl;;
 #define GLOBAL_NUMBERING "globalNumbering"
 #define SUBDOMAIN_ID "SubdomainId"
 #define FORMULATION_ID "FormulationId"
 #define MATERIAL_ID "MaterialId"
 #define DISPLACEMENT "displacement"
 
-
+#if DBG > 0
+#define HDDTRACES std::cout<< __FILE__ << ':'<<  __LINE__ <<'\n'<< std::flush;
+#else
+#define HDDTRACES ;
+#endif
